87503b5ff6e5c8fd784ddb760cc91d6949bde3e6
diederick <diederickc@gmail.com>
2024-04-07 22:33:04 +0200

    added send email = ERROR DeprecationWarning

--- emailing.py
+++ emailing.py
@@ -1,2 +1,28 @@
-def send_email():
-    print("Email was send")
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+PASSWORD = "lasv urpu neos nofg"
+SENDER = "diederickc@gmail.com"
+RECEIVER = "diederickc@gmail.com"
+
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New Customer showed up"
+    email_message.set_content("Hey, we just saw a new customer")
+
+    with open(image_path, 'rb') as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype='image', subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.send_message(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+
+if __name__ == '__main__':
+    send_email(image_path="images/19.png")
